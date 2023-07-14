fc4e18cca7ff46cab9f59626e9cb079b44db0bc1
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2023-07-14 16:38:03 +0300

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,44 @@
-document.addEventListener('mousemove', e => {
-	Object.assign(document.documentElement, {
-		style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
-		`
-	})
-})
+// document.addEventListener('mousemove', e => {
+// 	Object.assign(document.documentElement, {
+// 		style: `
+// 		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+// 		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+// 		`
+// 	})
+// })
 
-document.addEventListener('deviceorientation', e => {
-  const tiltX = e.beta; // Tilt around x-axis (forward/backward tilt)
-  const tiltY = e.gamma; // Tilt around y-axis (left/right tilt)
+// document.addEventListener('deviceorientation', e => {
+//   const tiltX = e.beta; // Tilt around x-axis (forward/backward tilt)
+// 	const tiltY = e.gamma; // Tilt around y-axis (left/right tilt)
+// 	const tiltZ = e.alpha; // Tilt around z-axis
+
+//   Object.assign(document.documentElement, {
+//     style: `
+//       --move-x: ${tiltX * -0.005}deg;
+//       --move-y: ${tiltY * 0.01}deg;
+//     `
+//   });
+// });
+function handleMove(e) {
+  let moveX, moveY;
+
+  if (e.type === 'mousemove') {
+    // Mouse movement
+    moveX = (e.clientX - window.innerWidth / 2) * -0.005;
+    moveY = (e.clientY - window.innerHeight / 2) * 0.01;
+  } else if (e.type === 'deviceorientation') {
+    // Device tilt
+    moveX = e.beta;
+    moveY = e.gamma;
+  }
 
   Object.assign(document.documentElement, {
     style: `
-      --move-x: ${tiltX * -0.005}deg;
-      --move-y: ${tiltY * 0.01}deg;
+      --move-x: ${moveX}deg;
+      --move-y: ${moveY}deg;
     `
   });
-});
+}
+
+document.addEventListener('mousemove', handleMove);
+window.addEventListener('deviceorientation', handleMove);
